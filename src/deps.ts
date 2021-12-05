633c262c523295444c5810899683748266a0289f
Johan Brook <johan@johanbrook.com>
2021-12-05 20:28:00 +0100

    Remove import map
    Doesn't work with Deno Deploy.

--- src/deps.ts
+++ src/deps.ts
@@ -1,8 +1,16 @@
-export { h, Fragment, createContext, hydrate } from 'preact';
-export type { ComponentChildren, VNode } from 'preact';
-export { useEffect, useState, useRef, useCallback, useReducer, useMemo, useContext } from 'preact/hooks';
-export type { Reducer } from 'preact/hooks';
-export { default as Mapbox } from 'mapboxgl';
-export { default as length } from 'turf-length';
-export { Application, Router, send } from 'oak';
-export { renderToString } from 'preact-render-to-string';
+export { h, Fragment, createContext, hydrate } from 'https://esm.sh/preact@10.6.1';
+export type { ComponentChildren, VNode } from 'https://esm.sh/preact@10.6.1';
+export {
+    useEffect,
+    useState,
+    useRef,
+    useCallback,
+    useReducer,
+    useMemo,
+    useContext,
+} from 'https://esm.sh/preact@10.6.1/hooks';
+export type { Reducer } from 'https://esm.sh/preact@10.6.1/hooks';
+export { default as Mapbox } from 'https://esm.sh/mapbox-gl@2';
+export { default as length } from 'https://esm.sh/@turf/length@6';
+export { Application, Router, send } from 'https://deno.land/x/oak@v10.0.0/mod.ts';
+export { renderToString } from 'https://esm.sh/preact-render-to-string@5?deps=preact@10.6.1';
